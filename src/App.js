d73f74e5996f7b77e00ec81fdf88f644e46e705e
Jeesoo Hong <jeesoo@dable.io>
2019-12-08 20:21:06 +0900

    picker 수정

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Switch>
-          <Route path="/intro" component={Intro} />
-          <Route path="/gallery" component={Gallery} />
-          {/* <Route path="/gallery/:id" component={Gallery} /> */}
-          <Route path="/" component={Intro} />
-        </Switch>
+    <>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route path="/intro" component={Intro} />
+            <Route path="/gallery" component={Gallery} />
+            {/* <Route path="/gallery/:id" component={Gallery} /> */}
+            <Route path="/" component={Intro} />
+          </Switch>
+        </div>
+      </Router>
+      <div className="hidden">
+        <img
+          src="./picker.png"
+          alt="picker"
+        />
       </div>
-    </Router>
+    </>
   );
 }
 
